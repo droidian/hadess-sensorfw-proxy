b60d9d3556134204f2ccbd4dacf499f838452376
Bastien Nocera <hadess@hadess.net>
2021-01-20 18:03:26 +0100

    utils: Add a g_auto() types to close file descriptors

--- src/utils.h
+++ src/utils.h
@@ -9,9 +9,16 @@
 
 #pragma once
 
+#include <stdio.h>
+#include <unistd.h>
 #include <glib.h>
 #include <gudev/gudev.h>
 
 #define IS_TEST (g_getenv ("UMOCKDEV_DIR") != NULL)
+
+typedef int IioFd;
+G_DEFINE_AUTO_CLEANUP_FREE_FUNC(IioFd, close, -1)
+
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(FILE, fclose)
 
 char *get_device_file (GUdevDevice *device);
